e3561ca2a51135150ef29a66510f3dab447d188c
MrCarefree <fpf159624753>
2019-12-04 16:03:24 +0700

    unfinished borrowing model

--- app/Borrowing.php
+++ app/Borrowing.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Borrowing extends Model
 {
-    //
+    protected $table = 'borrowings';
+    public $fillable = ['book_id', 'member_id', 'librarian_id', 'tanggal_peminjaman', 'tanggal_pengembalian', 'status'];
+
+    public function book(){
+        return;
+    }
 }
